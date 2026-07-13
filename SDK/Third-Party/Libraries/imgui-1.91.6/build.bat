30aefc5f9265342bbb8b6c2da3a5eb764c723a34
Hojin Lee (Joey / Unknown Stryker) <leehojin6580@gmail.com>
2026-07-14 00:59:55 +0900

    Update build.bat

--- SDK/Third-Party/Libraries/imgui-1.91.6/build.bat
+++ SDK/Third-Party/Libraries/imgui-1.91.6/build.bat
@@ -7,10 +7,10 @@ mkdir Solution_X64_AVX
 cd Solution_X64_AVX
 
 cmake -DCMAKE_SYSTEM_PROCESSOR=x64 -DCMAKE_SYSTEM_NAME=Windows -DCMAKE_CXX_STANDARD=20 .. -G %1 -T %2
-cmake --build . --config Debug -j8
-cmake --build . --config RelWithDebInfo -j8
-cmake --build . --config MinSizeRel -j8
-cmake --build . --config Release -j8
+cmake --build . --config Debug --parallel
+cmake --build . --config RelWithDebInfo --parallel
+cmake --build . --config MinSizeRel --parallel
+cmake --build . --config Release --parallel
 
 
 
@@ -20,9 +20,9 @@ mkdir Solution_X64_AVX512F
 cd Solution_X64_AVX512F
 
 cmake -DCMAKE_SYSTEM_PROCESSOR=x64 -DSIMD=AVX512F -DCMAKE_SYSTEM_NAME=Windows -DCMAKE_CXX_STANDARD=20 .. -G %1 -T %2
-cmake --build . --config Debug -j8
-cmake --build . --config RelWithDebInfo -j8
-cmake --build . --config MinSizeRel -j8
-cmake --build . --config Release -j8
+cmake --build . --config Debug --parallel
+cmake --build . --config RelWithDebInfo --parallel
+cmake --build . --config MinSizeRel --parallel
+cmake --build . --config Release --parallel
 
 endlocal
