d21a705141a33d84c9f54b26a160549bf62d571b
Hojin Lee (Joey / Unknown Stryker) <leehojin6580@gmail.com>
2026-09-03 03:31:36 +0900

    Update Tests: FE.pool.cpp

--- SDK/Tests/Unit-Tests/FE.pool.cpp
+++ SDK/Tests/Unit-Tests/FE.pool.cpp
@@ -4,6 +4,7 @@
 // std
 #include <iostream>
 #include <string>
+#include <random>
 
 // boost
 #include <boost/pool/object_pool.hpp>
@@ -14,6 +15,275 @@
 #include <FE/algorithm/utility.hxx>
 #include <FE/memory_resource.hxx>
 #include <FE/framework.hxx>
+
+
+
+
+void boost_object_pool_allocator_extreme_fixed_sized_accumulation_test(benchmark::State& state_p) noexcept
+{
+	std::string* l_strings[10000];
+	benchmark::DoNotOptimize(l_strings);
+
+	boost::object_pool<std::string> l_allocator;
+	benchmark::DoNotOptimize(l_allocator);
+	benchmark::ClobberMemory();
+
+	for (auto _ : state_p)
+	{
+		for (int i = 0; i < 500; ++i)
+		{
+			l_strings[i] = l_allocator.construct();
+		}
+
+		for (int i = 0; i < 500; ++i)
+		{
+			l_allocator.destroy(l_strings[i]);
+		}
+	}
+}
+BENCHMARK(boost_object_pool_allocator_extreme_fixed_sized_accumulation_test)->Iterations(1000);
+
+void boost_pool_allocator_extreme_fixed_sized_accumulation_test(benchmark::State& state_p) noexcept
+{
+	std::list<std::string, boost::pool_allocator<std::string, boost::default_user_allocator_new_delete, boost::details::pool::null_mutex>> l_strings;
+	benchmark::DoNotOptimize(l_strings);
+	benchmark::ClobberMemory();
+
+	for (auto _ : state_p)
+	{
+		for (int i = 0; i < 500; ++i)
+		{
+			l_strings.push_back(std::string());
+		}
+
+		for (int i = 0; i < 500; ++i)
+		{
+			l_strings.pop_back();
+		}
+	}
+}
+BENCHMARK(boost_pool_allocator_extreme_fixed_sized_accumulation_test)->Iterations(1000);
+
+void boost_fast_pool_allocator_extreme_fixed_sized_accumulation_test(benchmark::State& state_p) noexcept
+{
+	std::list<std::string, boost::fast_pool_allocator<std::string>> l_strings;
+	benchmark::DoNotOptimize(l_strings);
+	benchmark::ClobberMemory();
+
+	for (auto _ : state_p)
+	{
+		for (int i = 0; i < 500; ++i)
+		{
+			l_strings.push_back(std::string());
+		}
+
+		for (int i = 0; i < 500; ++i)
+		{
+			l_strings.pop_back();
+		}
+	}
+}
+BENCHMARK(boost_fast_pool_allocator_extreme_fixed_sized_accumulation_test)->Iterations(1000);
+
+
+
+void aligned_malloc_aligned_free_extreme_fixed_sized_accumulation_test(benchmark::State& state_p) noexcept
+{
+	std::list<std::string> l_strings;
+	benchmark::DoNotOptimize(l_strings);
+	benchmark::ClobberMemory();
+
+	for (auto _ : state_p)
+	{
+		for (int i = 0; i < 500; ++i)
+		{
+			l_strings.push_back(std::string());
+		}
+
+		for (int i = 0; i < 500; ++i)
+		{
+			l_strings.pop_back();
+		}
+	}
+}
+BENCHMARK(aligned_malloc_aligned_free_extreme_fixed_sized_accumulation_test)->Iterations(1000);
+
+void std_pmr_unsynchronized_pool_resource_extreme_fixed_sized_accumulation_test(benchmark::State& state_p) noexcept
+{
+	std::pmr::unsynchronized_pool_resource l_resource;
+	benchmark::DoNotOptimize(l_resource);
+
+	std::pmr::list<std::string> l_strings(&l_resource);
+	benchmark::DoNotOptimize(l_strings);
+	benchmark::ClobberMemory();
+
+	for (auto _ : state_p)
+	{
+		for (int i = 0; i < 500; ++i)
+		{
+			l_strings.push_back(std::string());
+		}
+
+		for (int i = 0; i < 500; ++i)
+		{
+			l_strings.pop_back();
+		}
+	}
+}
+BENCHMARK(std_pmr_unsynchronized_pool_resource_extreme_fixed_sized_accumulation_test)->Iterations(1000);
+
+
+void FE_TLGPMP_extreme_fixed_sized_accumulation_test(benchmark::State& state_p) noexcept
+{
+	std::pmr::list<std::string> l_strings(FE::framework::framework_base::get_framework().get_memory_resource());
+	benchmark::DoNotOptimize(l_strings);
+	benchmark::ClobberMemory();
+
+	for (auto _ : state_p)
+	{
+		for (int i = 0; i < 500; ++i)
+		{
+			l_strings.push_back(std::string());
+		}
+
+		for (int i = 0; i < 500; ++i)
+		{
+			l_strings.pop_back();
+		}
+	}
+}
+BENCHMARK(FE_TLGPMP_extreme_fixed_sized_accumulation_test)->Iterations(1000);
+
+void FE_LTLGPMP_extreme_fixed_sized_accumulation_test(benchmark::State& state_p) noexcept
+{
+	std::pmr::list<std::string> l_strings(FE::framework::framework_base::get_framework().get_large_memory_resource());
+	benchmark::DoNotOptimize(l_strings);
+	benchmark::ClobberMemory();
+
+	for (auto _ : state_p)
+	{
+		for (int i = 0; i < 500; ++i)
+		{
+			l_strings.push_back(std::string());
+		}
+
+		for (int i = 0; i < 500; ++i)
+		{
+			l_strings.pop_back();
+		}
+	}
+}
+BENCHMARK(FE_LTLGPMP_extreme_fixed_sized_accumulation_test)->Iterations(1000);
+
+
+
+
+void boost_object_pool_allocator_extreme_fixed_sized_test(benchmark::State& state_p) noexcept
+{
+	std::string* l_string;
+	benchmark::DoNotOptimize(l_string);
+
+	boost::object_pool<std::string> l_allocator;
+	benchmark::DoNotOptimize(l_allocator);
+	benchmark::ClobberMemory();
+
+	for (auto _ : state_p)
+	{
+		l_string = l_allocator.construct();
+		l_allocator.destroy(l_string);
+	}
+}
+BENCHMARK(boost_object_pool_allocator_extreme_fixed_sized_test)->Iterations(30000);
+
+void boost_pool_allocator_extreme_fixed_sized_test(benchmark::State& state_p) noexcept
+{
+	std::list<std::string, boost::pool_allocator<std::string, boost::default_user_allocator_new_delete, boost::details::pool::null_mutex>> l_strings;
+	benchmark::DoNotOptimize(l_strings);
+	benchmark::ClobberMemory();
+
+	for (auto _ : state_p)
+	{
+		l_strings.push_back(std::string());
+		l_strings.pop_back();
+	}
+}
+BENCHMARK(boost_pool_allocator_extreme_fixed_sized_test)->Iterations(30000);
+
+void boost_fast_pool_allocator_extreme_fixed_sized_test(benchmark::State& state_p) noexcept
+{
+	std::list<std::string, boost::fast_pool_allocator<std::string>> l_strings;
+	benchmark::DoNotOptimize(l_strings);
+	benchmark::ClobberMemory();
+
+	for (auto _ : state_p)
+	{
+		l_strings.push_back(std::string());
+		l_strings.pop_back();
+	}
+}
+BENCHMARK(boost_fast_pool_allocator_extreme_fixed_sized_test)->Iterations(30000);
+
+
+
+void aligned_malloc_aligned_free_extreme_fixed_sized_test(benchmark::State& state_p) noexcept
+{
+	std::list<std::string> l_strings;
+	benchmark::DoNotOptimize(l_strings);
+	benchmark::ClobberMemory();
+
+	for (auto _ : state_p)
+	{
+		l_strings.push_back(std::string());
+		l_strings.pop_back();
+	}
+}
+BENCHMARK(aligned_malloc_aligned_free_extreme_fixed_sized_test)->Iterations(30000);
+
+void std_pmr_unsynchronized_pool_resource_extreme_fixed_sized_test(benchmark::State& state_p) noexcept
+{
+	std::pmr::unsynchronized_pool_resource l_resource;
+	benchmark::DoNotOptimize(l_resource);
+
+	std::pmr::list<std::string> l_strings(&l_resource);
+	benchmark::DoNotOptimize(l_strings);
+	benchmark::ClobberMemory();
+
+	for (auto _ : state_p)
+	{
+		l_strings.push_back(std::string());
+		l_strings.pop_back();
+	}
+}
+BENCHMARK(std_pmr_unsynchronized_pool_resource_extreme_fixed_sized_test)->Iterations(30000);
+
+
+void FE_TLGPMP_extreme_fixed_sized_test(benchmark::State& state_p) noexcept
+{
+	std::pmr::list<std::string> l_strings(FE::framework::framework_base::get_framework().get_memory_resource());
+	benchmark::DoNotOptimize(l_strings);
+	benchmark::ClobberMemory();
+
+	for (auto _ : state_p)
+	{
+		l_strings.push_back(std::string());
+		l_strings.pop_back();
+	}
+}
+BENCHMARK(FE_TLGPMP_extreme_fixed_sized_test)->Iterations(30000);
+
+void FE_LTLGPMP_extreme_fixed_sized_test(benchmark::State& state_p) noexcept
+{
+	std::pmr::list<std::string> l_strings(FE::framework::framework_base::get_framework().get_large_memory_resource());
+	benchmark::DoNotOptimize(l_strings);
+	benchmark::ClobberMemory();
+
+	for (auto _ : state_p)
+	{
+		l_strings.push_back(std::string());
+		l_strings.pop_back();
+	}
+}
+BENCHMARK(FE_LTLGPMP_extreme_fixed_sized_test)->Iterations(30000);
 
 
 
@@ -284,3 +554,5 @@ void boost_fast_pool_allocator_random_size_accumulation_test(benchmark::State& s
 	}
 }
 BENCHMARK(boost_fast_pool_allocator_random_size_accumulation_test)->Iterations(10);
+
+#undef _MAX_ITERATION_
